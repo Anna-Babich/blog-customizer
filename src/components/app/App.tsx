d52c76a81502a9f80cf9a9276728c086349d8fe5
Anna-Babich <anbabi@yandex.ru>
2025-03-25 17:23:15 +0300

    Для ревью

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,17 @@
 import { CSSProperties, useState } from 'react';
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { ArticleStateType, defaultArticleState } from '../../constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from '../../constants/articleProps';
 
 import '../../styles/index.scss';
 import styles from '../../styles/index.module.scss';
 
 export const App = () => {
-	const [articalState, setArticalState] = useState<ArticleStateType>(defaultArticleState);
+	const [articalState, setArticalState] =
+		useState<ArticleStateType>(defaultArticleState);
 	return (
 		<main
 			className={styles.main}
@@ -20,7 +24,12 @@ export const App = () => {
 					'--bg-color': articalState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm stateArticleParamsForm={articalState} setParamsFormState={setArticalState} articalParamsFormTitle="Задайте параметры" as="h2" />
+			<ArticleParamsForm
+				stateArticleParamsForm={articalState}
+				setParamsFormState={setArticalState}
+				articalParamsFormTitle='Задайте параметры'
+				as='h2'
+			/>
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,21 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { Select } from 'src/ui/select';
 import { useState, useRef, ElementType } from 'react';
-import { fontFamilyOptions, fontColors, fontSizeOptions, backgroundColors, contentWidthArr, defaultArticleState, ArticleStateType, OptionType } from '../../constants/articleProps';
+import {
+	fontFamilyOptions,
+	fontColors,
+	fontSizeOptions,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+} from '../../constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Text } from 'src/ui/text';
 import { Separator } from 'src/ui/separator';
 import { useOutsideClickClose } from '../../ui/select/hooks/useOutsideClickClose';
 import clsx from 'clsx';
-
 
 import styles from './ArticleParamsForm.module.scss';
 
@@ -17,9 +25,14 @@ type ArticleParamsForm = {
 	setParamsFormState: (data: ArticleStateType) => void;
 	articalParamsFormTitle: string;
 	as: ElementType;
-}
+};
 
-export const ArticleParamsForm = ({stateArticleParamsForm, setParamsFormState, articalParamsFormTitle, as}: ArticleParamsForm) => {
+export const ArticleParamsForm = ({
+	stateArticleParamsForm,
+	setParamsFormState,
+	articalParamsFormTitle,
+	as,
+}: ArticleParamsForm) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const rootRef = useRef<HTMLDivElement>(null);
 	//хук для закрытия сайдбара при клике на оверлей
@@ -27,65 +40,87 @@ export const ArticleParamsForm = ({stateArticleParamsForm, setParamsFormState, a
 		isOpen,
 		rootRef,
 		onChange: setIsOpen,
-	})
+	});
 
-	const [selectParamsState, setSelectParamsState] = useState<ArticleStateType>(stateArticleParamsForm);
-	const handeleChangeSelectedState = (key: keyof ArticleStateType, value: OptionType) => {
-		setSelectParamsState({...selectParamsState, [key]: value});
-	}
+	const [selectParamsState, setSelectParamsState] = useState<ArticleStateType>(
+		stateArticleParamsForm
+	);
+	const handeleChangeSelectedState = (
+		key: keyof ArticleStateType,
+		value: OptionType
+	) => {
+		setSelectParamsState({ ...selectParamsState, [key]: value });
+	};
 
-	const handleSubmitStateArtical = (event: React.FormEvent<HTMLFormElement>) => {
+	const handleSubmitStateArtical = (
+		event: React.FormEvent<HTMLFormElement>
+	) => {
 		event.preventDefault();
 		setParamsFormState(selectParamsState);
-	}
+	};
 
 	const handleResetStateArtical = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		setParamsFormState(defaultArticleState);
 		setSelectParamsState(defaultArticleState);
-	}
+	};
 
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
-			<aside className={clsx(styles.container, isOpen && styles.container_open)} ref={rootRef} >
-				<form className={styles.form} onSubmit={handleSubmitStateArtical} onReset={handleResetStateArtical}>
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				ref={rootRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmitStateArtical}
+					onReset={handleResetStateArtical}>
 					<Text as={as} size={31} weight={800} uppercase>
 						{articalParamsFormTitle}
 					</Text>
-					<Select 
-						selected={selectParamsState.fontFamilyOption} 
+					<Select
+						selected={selectParamsState.fontFamilyOption}
 						options={fontFamilyOptions}
-						onChange={(options) => handeleChangeSelectedState('fontFamilyOption', options)}
-						title={'шрифты'} 
+						onChange={(options) =>
+							handeleChangeSelectedState('fontFamilyOption', options)
+						}
+						title={'шрифты'}
 					/>
-					<RadioGroup 
-						name={'radio'} 
-						options={fontSizeOptions} 
+					<RadioGroup
+						name={'radio'}
+						options={fontSizeOptions}
 						selected={selectParamsState.fontSizeOption}
-						onChange={(options) => handeleChangeSelectedState('fontSizeOption', options)}
-						title={'размер шрифта'} 
+						onChange={(options) =>
+							handeleChangeSelectedState('fontSizeOption', options)
+						}
+						title={'размер шрифта'}
 					/>
-					<Select 
-						selected={selectParamsState.fontColor} 
+					<Select
+						selected={selectParamsState.fontColor}
 						options={fontColors}
-						onChange={(options) => handeleChangeSelectedState('fontColor', options)}
+						onChange={(options) =>
+							handeleChangeSelectedState('fontColor', options)
+						}
 						title={'цвет шрифта'}
 					/>
-					
+
 					<Separator />
 
-					<Select 
-						selected={selectParamsState.backgroundColor} 
+					<Select
+						selected={selectParamsState.backgroundColor}
 						options={backgroundColors}
-						onChange={(options) => handeleChangeSelectedState('backgroundColor', options)}
-						title={'Цвет фона'} 
+						onChange={(options) =>
+							handeleChangeSelectedState('backgroundColor', options)
+						}
+						title={'Цвет фона'}
 					/>
-					<Select 
-						selected={selectParamsState.contentWidth} 
+					<Select
+						selected={selectParamsState.contentWidth}
 						options={contentWidthArr}
-						onChange={(options) => handeleChangeSelectedState('contentWidth', options)}
-						title={'Ширина контента'} 
+						onChange={(options) =>
+							handeleChangeSelectedState('contentWidth', options)
+						}
+						title={'Ширина контента'}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
